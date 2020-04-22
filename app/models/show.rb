b14d903675cc6feecfa6dcac43af1187d5bf9f90
Derric Fragosa <fderric@gmail.com>
2020-04-22 19:46:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
+
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :shows
+  belongs_to :network
   
+  def actors_list
+    self.actors.map{|actor| actor.full_name}
+  end
 end
